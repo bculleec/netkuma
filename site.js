294bf568a5153a1145a0654974c69f88dc8e6e83
bculleec <bculleec@uoguelph.ca>
2025-11-18 22:53:20 -0500

    adding deployment site

---
+++ site.js
@@ -0,0 +1,24 @@
+import { netkuma } from './netkuma.js';
+
+const app = netkuma({ logger: true , publicDir: 'site'});
+
+app.get('/', (request, reply) => {
+    return reply.send("Super fast API!!!");
+});
+
+app.get('/feed', (request, reply) => {
+    return reply.send("Welcome to your feed.");
+});
+
+/* you can even render an html page! */
+app.get('/about', (request, reply) => {
+    return reply.view('about.html');
+});
+
+app.get('/docs', (request, reply) => {
+    return reply.send("This page is under construction!");
+})
+
+app.listen({ port: 8000, host: '0.0.0.0' }, (err, address) => {
+    if (err) throw err;
+});
